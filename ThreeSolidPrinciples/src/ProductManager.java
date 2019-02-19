de4e75b744a4d3f411ef0f0a176582a0d3554fe0
KProft <kcp36@txstate.edu>
2019-02-18 19:29:45 -0600

    updated chronology

---
+++ ThreeSolidPrinciples/src/ProductManager.java
@@ -0,0 +1,9 @@
+//Karen Proft
+package ThreeSolidPrinciples;
+
+public class ProductManager extends Manager{
+    public void defineproduct()
+	{
+		System.out.format("Product manager is now defining products\n");
+	}
+}

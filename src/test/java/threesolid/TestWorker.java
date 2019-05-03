1960fb35822f7e7f4e46d854fd17b9b83f487398
KProft <kcp36@txstate.edu>
2019-05-03 18:15:08 -0500

    added my tests

--- src/test/java/threesolid/TestWorker.java
+++ src/test/java/threesolid/TestWorker.java
@@ -5,13 +5,29 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TestWorker {
-    
-        private Worker w = new Worker();
+    private Manager m = new Manager();
+    private Worker w = new Worker();
 
 	@DisplayName("Work Method Test")
 	@Test
 	public void testWorkMethod() {
              assertEquals(w.work(),"I am a worker. \n So you should pay me. \n", "Mismatch between test text and method text");
+
+   }
+   
+   @DisplayName("Karen's Worker Pass")
+	@Test
+	public void newtestKCPWorkerPass() {
+			 
+             assertNull(m.worker, "Manager was incorrectly assigned a worker to start");
+
+   }
+   
+   @DisplayName("Karen's Worker Fail")
+	@Test
+	public void newtestKCPWorkerFail() {
+             m.setWorker(w);
+			 assertTrue(m.worker == null, "Manager should not have been assigned a worker");
 
    }
 
